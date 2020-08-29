5d9bd642db2421cc194a7e597dd06dff6e1c4340
System Administrator <vemulashankar753@gmail.com>
2020-08-28 22:56:51 -0400

    changes for polymorphism

--- cppprograms/Inher/NoPoly.cpp
+++ cppprograms/Inher/NoPoly.cpp
@@ -2,8 +2,12 @@
 //Without using the Virtual key word, when the makeFishSwim  is called, tuna object will remain as fish object
 //here when the tuna object is passed as a reference to makeFishSwim, instread of it converts to Tuna, it remains as Fish.
 //If we use Virtual, keyword it will be solved.
+
+//Even if we use smart pointer, in this case still we will have static binding
+
 #include <iostream>
 #include <string>
+#include <memory>
 
 using namespace std;
 
@@ -46,6 +50,12 @@ int main(){
  f1->swim(); //As there is no Virtual key word, there is no Dynamic ploymorphism is achieved,
            // It still calls the Base class swim.
  delete f1;
+
+ std::unique_ptr<Fish> ptr1 = std::make_unique<Tuna>();
+
+ ptr1->swim();
+
+ //Need not to do delete, as it is smart pointer, it will be deleted automatically after it goes out of scope.
 
 }
 
